7e9790cbba534a66ac78cb31b5f6374ca0ec6423
Tommy Xaypanya <Tommy.Xaypanya.contractor@hilton.com>
2019-07-26 16:42:53 -0500

    Ready to Go Friday

--- src/core/registry.js
+++ src/core/registry.js
@@ -1,15 +1,15 @@
 'use strict'
 
-const http-validation_NAMESPACE = '_registredExtensions'
+const HTTPVALIDATION_NAMESPACE = '_registredExtensions'
 
 /**
  * Registers an extension.
  *
  * @param {Object} world       - Cucumber world object
- * @param {string} extensionId - Unique http-validation extension identifier
+ * @param {string} extensionId - Unique HTTPVALIDATION extension identifier
  */
 exports.registerExtension = (world, extensionId) => {
-    world._registredExtensions = world[http-validation_NAMESPACE] || []
+    world._registredExtensions = world[HTTPVALIDATION_NAMESPACE] || []
     world._registredExtensions.push(extensionId)
 }
 
@@ -17,12 +17,12 @@ exports.registerExtension = (world, extensionId) => {
  * Checks if an extension were registered.
  *
  * @param {Object} world       - Cucumber world object
- * @param {string} extensionId - Unique http-validation extension identifier
+ * @param {string} extensionId - Unique HTTPVALIDATION extension identifier
  * @return {boolean}
  */
 exports.hasExtension = (world, extensionId) => {
-    if (!world[http-validation_NAMESPACE]) return false
-    if (!world[http-validation_NAMESPACE].includes(extensionId)) return false
+    if (!world[HTTPVALIDATION_NAMESPACE]) return false
+    if (!world[HTTPVALIDATION_NAMESPACE].includes(extensionId)) return false
 
     return true
 }
